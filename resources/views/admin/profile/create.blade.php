4ce879649421b8e8610e0f1fab07640d79049d9e
EC2 Default User <ec2-user@ip-172-31-34-186.us-east-2.compute.internal>
2019-06-14 07:45:04 +0000

    PHP/Laravel 11 Bladeテンプレートの継承とLaravel Mixの使い方を活用してみよう 課題3〜8

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,3 +1,4 @@
+<!--
 <!DOCTYPE html>
 <html>
     <head>
@@ -10,3 +11,21 @@
         <h1>Myプロフィール作成</h1>
     </body>
 </html>
+-->
+
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
+
+{{-- profile.blade.phpの@yield('title')に以下のタグを埋め込む --}}
+@section('title', 'My プロフィール')
+
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>My プロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
